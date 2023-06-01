28ae2fdc4d04fa73ef6bc7e190373e640ce235b7
Sophie Sun <swl381029812@gmail.com>
2023-06-01 15:42:36 +1000

    Add more items

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Shopping cart project with React and Typescript
+
+# Run the Project
+
+Please type "npm run dev" to start the project
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
+import homepageImg from "../assets/imgs/homepage.avif";
+
 function Home() {
   return (
-    <h1>
-      Home
-    </h1>
-  )
+    <>
+      <h1>Home Page - <em>Please navigate to store page to select products</em></h1>
+      <img src={homepageImg} width="1400px"></img>
+    </>
+  );
 }
 
 export default Home;
